1791e11828548440e90c2b3d7497b46c85015b81
julicq <julicq@gmail.com>
2022-09-07 14:23:57 +0300

    add variety to questions

--- quizz.py
+++ quizz.py
@@ -1,5 +1,7 @@
+import random
 from string import ascii_lowercase
 
+NUM_QUESTIONS_PER_QUIZ = 5
 QUESTIONS = {
     "What was Meta Platforms Inc formerly known as": [
         "Facebook", "Google", "Apple", "Dell"
@@ -9,15 +11,25 @@ QUESTIONS = {
     ],
     "Which former British colony was given back to China in 1997": [
         "Hong Kong", "Shanghai", "Beijing", "Wuhan"
+    ],
+    "The logo for luxury car maker Porsche features which animal": [
+        "Horse", "Goat", "Eagle", "Snake"
+    ],
+    "Which element is said to keep bones strong": [
+        "Calcium", "Chromium", "Titanium", "Helium"
     ]
 }
 
+num_questions = min(NUM_QUESTIONS_PER_QUIZ, len(QUESTIONS))
+questions = random.sample(list(QUESTIONS.items()), k=num_questions)
+
 num_correct = 0
-for num, (question, alternatives) in enumerate(QUESTIONS.items(), start=1):
+for num, (question, alternatives) in enumerate(questions, start=1):
     print(f"\nQuestion {num}:")
     print(f"{question}?")
     correct_answer = alternatives[0]
-    labeled_alternatives = dict(zip(ascii_lowercase, sorted(alternatives)))
+    labeled_alternatives = dict(
+        zip(ascii_lowercase, random.sample(alternatives, k=len(alternatives))))
     for label, alternative in labeled_alternatives.items():
         print(f"  {label} - {alternative}")
 

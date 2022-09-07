7e872bd1432cf625c98f3e39bcbed597590fca14
julicq <julicq@gmail.com>
2022-09-07 14:40:02 +0300

    refactor into separate functions

--- quizz.py
+++ quizz.py
@@ -20,27 +20,53 @@ QUESTIONS = {
     ]
 }
 
-num_questions = min(NUM_QUESTIONS_PER_QUIZ, len(QUESTIONS))
-questions = random.sample(list(QUESTIONS.items()), k=num_questions)
 
-num_correct = 0
-for num, (question, alternatives) in enumerate(questions, start=1):
-    print(f"\nQuestion {num}:")
-    print(f"{question}?")
+def run_quizz():
+    # Preprocess
+    questions = prepare_questions(
+        QUESTIONS, num_questions=NUM_QUESTIONS_PER_QUIZ
+    )
+
+    # Process (main loop)
+    num_correct = 0
+    for num, (question, alternatives) in enumerate(questions, start=1):
+        print(f"\nQuestion {num}:")
+        num_correct += ask_question(question, alternatives)
+
+    # Postprocess
+    print(f"\nYou got {num_correct} correct out of {num} questions")
+
+
+def prepare_questions(questions, num_questions):
+    num_questions = min(num_questions, len(questions))
+    return random.sample(list(questions.items()), k=num_questions)
+
+
+def ask_question(question, alternatives):
     correct_answer = alternatives[0]
-    labeled_alternatives = dict(
-        zip(ascii_lowercase, random.sample(alternatives, k=len(alternatives))))
-    for label, alternative in labeled_alternatives.items():
-        print(f"  {label} - {alternative}")
+    ordered_alternatives = random.sample(alternatives, k=len(alternatives))
 
-    while(answer_label := input("\nChoice? ")) not in labeled_alternatives:
-        print(f"Please answer one of {', '.join(labeled_alternatives)}")
-
-    answer = labeled_alternatives[answer_label]
+    answer = get_answer(question, ordered_alternatives)
     if answer == correct_answer:
-        num_correct += 1
-        print("⭐ Correct! ⭐")
+        print("* Correct! *")
+        return 1
     else:
         print(f"The answer is {correct_answer!r}, not {answer!r}")
+        return 0
 
-print(f"\nYou got {num_correct} correct out of {num} questions")
+
+
+def get_answer(question, alternatives):
+    print(f"{question}?")
+    labeled_alternatives = dict(zip(ascii_lowercase, alternatives))
+    for label, alternative in labeled_alternatives.items():
+        print(f"   {label})  {alternative}")
+
+    while (answer_label := input("\nChoice?")) not in labeled_alternatives:
+        print(f"Please answer one of {', '.join(labeled_alternatives)}")
+
+    return labeled_alternatives[answer_label]
+
+
+if __name__ == "__main__":
+    run_quizz()

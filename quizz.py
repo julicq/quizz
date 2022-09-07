8f79ea30604cc4311a445f5361b0a1ac2e64d933
julicq <julicq@gmail.com>
2022-09-07 14:58:07 +0300

    move questions to TOML file, code correction

--- quizz.py
+++ quizz.py
@@ -1,59 +1,49 @@
 import random
 from string import ascii_lowercase
+import pathlib
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
 
 NUM_QUESTIONS_PER_QUIZ = 5
-QUESTIONS = {
-    "What was Meta Platforms Inc formerly known as": [
-        "Facebook", "Google", "Apple", "Dell"
-    ],
-    "Which English city is known as the Steel City": [
-        "Sheffield", "London", "Manchester", "Liverpool"
-    ],
-    "Which former British colony was given back to China in 1997": [
-        "Hong Kong", "Shanghai", "Beijing", "Wuhan"
-    ],
-    "The logo for luxury car maker Porsche features which animal": [
-        "Horse", "Goat", "Eagle", "Snake"
-    ],
-    "Which element is said to keep bones strong": [
-        "Calcium", "Chromium", "Titanium", "Helium"
-    ]
-}
+QUESTIONS_PATH = pathlib.Path(__file__).parent / "questions.toml"
 
 
 def run_quizz():
     # Preprocess
     questions = prepare_questions(
-        QUESTIONS, num_questions=NUM_QUESTIONS_PER_QUIZ
+        QUESTIONS_PATH, num_questions=NUM_QUESTIONS_PER_QUIZ
     )
 
     # Process (main loop)
     num_correct = 0
-    for num, (question, alternatives) in enumerate(questions, start=1):
+    for num, question in enumerate(questions, start=1):
         print(f"\nQuestion {num}:")
-        num_correct += ask_question(question, alternatives)
+        num_correct += ask_question(question)
 
     # Postprocess
     print(f"\nYou got {num_correct} correct out of {num} questions")
 
 
-def prepare_questions(questions, num_questions):
+def prepare_questions(path, num_questions):
+    questions = tomllib.loads(path.read_text())["questions"]
     num_questions = min(num_questions, len(questions))
-    return random.sample(list(questions.items()), k=num_questions)
+    return random.sample(questions, k=num_questions)
 
 
-def ask_question(question, alternatives):
-    correct_answer = alternatives[0]
+def ask_question(question):
+    correct_answer = question["answer"]
+    alternatives = [question["answer"]] + question["alternatives"]
     ordered_alternatives = random.sample(alternatives, k=len(alternatives))
 
-    answer = get_answer(question, ordered_alternatives)
+    answer = get_answer(question["question"], ordered_alternatives)
     if answer == correct_answer:
         print("* Correct! *")
         return 1
     else:
         print(f"The answer is {correct_answer!r}, not {answer!r}")
         return 0
-
 
 
 def get_answer(question, alternatives):
